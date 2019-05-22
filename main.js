8c55760b0bd813a5a2e74be89798e8deb7da8311
agata <toshitaka.agata@gmail.com>
2019-05-22 23:50:59 +0900

    can not select file using <input type="file"> on windows

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input/file')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
